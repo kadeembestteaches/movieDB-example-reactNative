f08fc220747ce8ee23388b94f8c998043405ad4e
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-08-27 13:50:09 -0400

    MovieDB Website

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { StyleSheet, View,Image,FlatList } from 'react-native';
 
-export default function App() {
+const  App = ()=> {
+ 
+  const [movies, setMovies] =useState([]);
+
+  useEffect(()=>{
+    //Send a request to consume the RESTFul API 
+
+    fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=ab97473b7193d23cef553c0e9ef015c8&language=en-US&page=1")
+    .then(res=>res.json())
+    .then(data=>{
+      
+      console.log(data.results);
+      setMovies(data.results)
+    })
+    .catch(err=>console.log(`Error ${error}`));
+
+
+  },[]);
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      
+      <FlatList
+            data={movies}
+            renderItem={({item})=>(
+
+                <View key={item.id}  style={styles.container}>
+                    <Image 
+                    source={{uri:`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${item.poster_path}`}} 
+                    resizeMode="cover"  style={{width:380, height:500}}  />
+                </View>
+            )
+        }
+      
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  movieImage:
+  {
+    marginBottom:10,
+    paddingLeft:16,
+    paddingRight:16
+  }
+
 });
+
+export default App;
+
